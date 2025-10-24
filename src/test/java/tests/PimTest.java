ee429600ecb904c31eab15f5dcb6c9f6eb18f5c4
siwar99 <siwarsahraoui08@gmail.com>
2025-10-24 20:27:29 +0100

    add tests

--- src/test/java/tests/PimTest.java
+++ src/test/java/tests/PimTest.java
@@ -36,4 +36,26 @@ public class PimTest extends BaseTest {
         TestData.PIM_EXPECTED_EMPLOYEE_NAME,
         actualName);
   }
+
+  @Test
+  public void testSearchEmployeeNoResults() {
+    // 1. Login
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.navigateToLoginPage();
+    loginPage.login(TestData.VALID_USERNAME, TestData.VALID_PASSWORD);
+
+    // 2. Navigate to PIM page
+    DashboardPage dashboardPage = new DashboardPage(driver);
+    dashboardPage.clickPimLink();
+
+    // 3. Search for non-existent employee
+    PimPage pimPage = new PimPage(driver);
+    assertTrue("PIM page should be displayed", pimPage.isPimPageDisplayed());
+
+    pimPage.searchForEmployee(TestData.NONEXISTENT_EMPLOYEE_NAME);
+
+    // 4. Verify no results found using the new method
+    assertTrue("No records should be found for non-existent employee",
+            pimPage.isNoRecordsFound());
+  }
 }
